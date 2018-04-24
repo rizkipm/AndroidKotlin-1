7af6948b5c1d7ac11761256a6b5fd6a1951d98cb
Programmer Muslim <rizki@imastudio.co.id>
2018-04-24 13:57:42 +0700

    add

--- app/src/main/java/superheroku/id/co/androidkotlin1/MainActivity.kt
+++ app/src/main/java/superheroku/id/co/androidkotlin1/MainActivity.kt
@@ -2,11 +2,19 @@ package superheroku.id.co.androidkotlin1
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textNama = findViewById(R.id.txtNama) as EditText
+        val buttonSubmit = findViewById(R.id.btnSubmit) as Button
+        buttonSubmit.setOnClickListener {
+            Toast.makeText(this, "Hi dude i'm " + textNama.text, Toast.LENGTH_SHORT).show()
+        }
     }
 }
